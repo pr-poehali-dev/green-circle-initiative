dcb55798f016fa0b14332a3f3ba3102c58268928
qanelph <dd@ddostup.ru>
2025-04-28 00:31:57 +0300

    Увеличение номера страницы на единицу (восстановлено из af77ce1)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
-  const [count, setCount] = useState(51);
+  const [count, setCount] = useState(50);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
