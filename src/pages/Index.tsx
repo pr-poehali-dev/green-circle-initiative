45989e4e6b2b6aa2dc07bcff5a890f4234a00404
qanelph <dd@ddostup.ru>
2025-04-27 23:19:05 +0300

    Увеличение значения на единицу (восстановлено из 5c8729b)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
-  const [count, setCount] = useState(38);
+  const [count, setCount] = useState(41);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
