f557a44b0a96897ccd9f71b265ff74d759b00437
qanelph <dd@ddostup.ru>
2025-04-27 23:12:02 +0300

    Инкремент номера страницы (восстановлено из c4294ac)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
-  const [count, setCount] = useState(40);
+  const [count, setCount] = useState(39);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
