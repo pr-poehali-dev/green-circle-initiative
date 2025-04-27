b7df31a893b6dbf083c0845bd52c615d895fa170
qanelph <dd@ddostup.ru>
2025-04-27 22:33:37 +0300

    Добавлено число +1 в счетчик (восстановлено из 9156f7d)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
-  const [count, setCount] = useState(27);
+  const [count, setCount] = useState(24);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
