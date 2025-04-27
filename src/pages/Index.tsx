c286ffb2765cdb6cfe7c95c4d344f61b18ff4183
qanelph <dd@ddostup.ru>
2025-04-27 22:32:32 +0300

    Увеличить счетчик на единицу (восстановлено из 33e0cef)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const Index = () => {
-  const [count, setCount] = useState(30);
+  const [count, setCount] = useState(29);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
