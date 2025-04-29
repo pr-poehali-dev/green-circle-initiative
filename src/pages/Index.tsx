c63daf90489c46a9e7dcf8501057b84ee425264a
qanelph <dd@ddostup.ru>
2025-04-29 23:05:59 +0300

    Добавить градиентный фон (восстановлено из 2943df6)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,8 +11,8 @@ const Index = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center p-8 bg-white rounded-lg shadow-md relative">
-        {/* Белый круг */}
-        <div className="w-24 h-24 bg-white rounded-full absolute -top-12 left-1/2 transform -translate-x-1/2 shadow-lg border border-gray-200"></div>
+        {/* Градиентный круг */}
+        <div className="w-24 h-24 rounded-full absolute -top-12 left-1/2 transform -translate-x-1/2 shadow-lg bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500"></div>
         
         <h1 className="text-4xl font-bold mb-4 text-black mt-8">Число + 1</h1>
         <div className="text-6xl font-bold my-8 text-purple-600">{count}</div>
